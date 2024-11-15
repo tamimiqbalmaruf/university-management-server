bb9094048f431689f76de5ad1204891e474590ad
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-15 10:14:04 +0600

    Created index route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
-import { dbConnect } from './utils/dbConnect';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
+import router from './app/routes';
+import { dbConnect } from './utils/dbConnect';
 
 const app: Application = express();
 
@@ -16,17 +15,14 @@ app.use(cors());
 dbConnect();
 
 // application routes
-app.use('/api/v1/users', UserRoutes);
-app.use('/api/v1/students', StudentRoutes);
+app.use('/api/v1', router);
 
-const getController = (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: 'Welcome to the API of first project',
   });
-};
-
-app.get('/', getController);
+});
 
 app.use(globalErrorHandler);
 
