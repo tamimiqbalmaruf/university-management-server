b10ba29f886793f2b794c1afa27ddde469f351b9
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-20 21:17:25 +0600

    generateStudent bug fixed

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -10,12 +10,24 @@ const findLastStudentId = async () => {
         .lean();
 
 
-    return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+    return lastStudent?.id ? lastStudent.id : undefined;
 }
 
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-    const currentId = await findLastStudentId() || (0).toString();
+    let currentId = (0).toString(); // 0000 by default
+
+    const lastStudentId = await findLastStudentId();
+    const lastStudentSemesterCode = lastStudentId?.substring(4,6); // 01
+    const lastStudentYear = lastStudentId?.substring(0, 4) // 2030
+
+    const currentSemesterCode = payload.code;
+    const currentYear = payload.year;
+
+    if(lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentYear === currentYear){
+        currentId = lastStudentId.substring(6); // 0001
+    };
+
     let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
 
     incrementId = `${payload.year}${payload.code}${incrementId}`
