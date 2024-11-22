f7ad18d9ed4ff2602b73f79524dbb51b41c69381
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-22 12:23:40 +0600

    Delete student using transaction & rollback

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,8 +1,12 @@
+import mongoose from 'mongoose';
 import { AcademicDepartment } from '../academicDepartment/academicDepartment.model';
 import { AcademicFaculty } from '../academicFaculty/academicFaculty.model';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TStudent } from './student.interface';
 import { Student } from './student.model';
+import AppError from '../../errors/AppError';
+import { StatusCodes } from 'http-status-codes';
+import { User } from '../user/user.model';
 
 
 
@@ -37,8 +41,54 @@ const getSingleStudentFromDB = async (id: string) => {
 };
 
 const deleteStudentFromDB = async (id: string) => {
-  const result = await Student.updateOne({ id }, { isDeleted: true });
-  return result;
+
+
+  const isUserExists = await Student.isUserExists(id);
+
+  if(!isUserExists){
+    throw new AppError(StatusCodes.BAD_REQUEST, "This user doesn't exist")
+  }
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+
+    const deletedStudent = await Student.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session }
+    );
+
+
+    if (!deletedStudent) {
+      throw new AppError(StatusCodes.BAD_REQUEST, "Failed to delete student")
+    };
+
+
+    const deletedUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session }
+    );
+
+
+    if (!deletedUser) {
+      throw new AppError(StatusCodes.BAD_REQUEST, "Failed to delete user")
+    };
+
+    await session.commitTransaction();
+    await session.endSession()
+
+    return deletedStudent;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+  }
+
+
+
 };
 
 export const StudentServices = {
