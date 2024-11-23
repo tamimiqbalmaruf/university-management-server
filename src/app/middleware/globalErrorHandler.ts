5ad5c09c0c7a97d05771e26d726c804a16130237
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-23 11:18:28 +0600

    handle Error , AppError UnhandledRejection _ UncaughtException

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -6,18 +6,16 @@ import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationError';
 import handleCastError from '../errors/handleCastError';
 import handleDuplicateError from '../errors/handleDuplicateError';
+import AppError from '../errors/AppError';
 
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    let statusCode = err.statusCode || 500;
-    let message = err.message || "Something went wrong!";
+    let statusCode = 500;
+    let message = "Something went wrong!";
     let errorSources: TErrorSources = [{
         path: "",
         message: "Something went wrong!"
     }]
-
-
-
 
 
     if (err instanceof ZodError) {
@@ -31,18 +29,24 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
         statusCode = simplifiedError?.statusCode;
         message = simplifiedError?.message;
         errorSources = simplifiedError?.errorSources;
-    }else if (err?.name === "CastError") {
+    } else if (err?.name === "CastError") {
         const simplifiedError = handleCastError(err);
         statusCode = simplifiedError?.statusCode;
         message = simplifiedError?.message;
         errorSources = simplifiedError?.errorSources;
-    }else if (err?.code === "11000") {
+    } else if (err?.code === 11000) {
         const simplifiedError = handleDuplicateError(err);
         statusCode = simplifiedError?.statusCode;
         message = simplifiedError?.message;
         errorSources = simplifiedError?.errorSources;
+    } else if (err instanceof AppError) {
+        statusCode = err?.statusCode;
+        message = err?.message;
+        errorSources = [{ path: "", message: err?.message }]
+    } else if (err instanceof Error) {
+        message = err?.message;
+        errorSources = [{ path: "", message: err?.message }]
     }
-
 
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import app from './app';
 const port = process.env.PORT || 5000;
+import { Server } from "http";
+
+let server: Server;
 
 const startServer = async () => {
   try {
-    app.listen(port, () => {
+    server = app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
@@ -12,3 +15,16 @@ const startServer = async () => {
 };
 
 startServer();
+
+process.on("unhandledRejection", () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1)
+    })
+  }
+  process.exit(1)
+});
+
+process.on("uncaughtException", () => {
+  process.exit(1)
+});
