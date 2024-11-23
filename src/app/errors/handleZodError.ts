646834a4836288a187524be58314cd18102262e0
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-23 10:34:30 +0600

    convert mongoose validation error

---
+++ src/app/errors/handleZodError.ts
@@ -0,0 +1,23 @@
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSources } from "../interface/error";
+
+ const handleZodError = (err: ZodError) => {
+
+    const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+        return {
+            path: issue?.path[issue.path.length - 1],
+            message: issue.message
+        }
+    })
+
+
+    const statusCode = 400;
+
+    return {
+        statusCode,
+        message: "Validation Error",
+        errorSources
+    }
+};
+
+export default handleZodError;
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,45 +2,32 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { ZodError, ZodIssue } from 'zod';
 import { TErrorSources } from '../interface/error';
 import config from '../config';
+import handleZodError from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     let statusCode = err.statusCode || 500;
     let message = err.message || "Something went wrong!";
-
-
-
     let errorSources: TErrorSources = [{
         path: "",
         message: "Something went wrong!"
     }]
 
 
-    const handleZodError = (err: ZodError) => {
 
-        const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
-            return {
-                path: issue?.path[issue.path.length - 1],
-                message: issue.message
-            }
-        })
-
-
-        const statusCode = 400;
-
-        return {
-            statusCode,
-            message: "Validation Error",
-            errorSources
-        }
-    };
 
 
     if (err instanceof ZodError) {
         const simplifiedError = handleZodError(err);
-statusCode = simplifiedError?.statusCode;
-message = simplifiedError?.message;
-errorSources = simplifiedError?.errorSources;
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSources = simplifiedError?.errorSources;
 
+    }else if(err?.name === "ValidationError"){
+        const simplifiedError = handleValidationError(err);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSources = simplifiedError?.errorSources;
     }
 
 
