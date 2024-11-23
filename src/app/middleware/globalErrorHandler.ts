12effc089f29633a1b7264227bcefc3962f2efcf
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-23 11:02:38 +0600

    handle castError and 11000 error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -4,6 +4,9 @@ import { TErrorSources } from '../interface/error';
 import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
+
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     let statusCode = err.statusCode || 500;
@@ -23,8 +26,18 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
         message = simplifiedError?.message;
         errorSources = simplifiedError?.errorSources;
 
-    }else if(err?.name === "ValidationError"){
+    } else if (err?.name === "ValidationError") {
         const simplifiedError = handleValidationError(err);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSources = simplifiedError?.errorSources;
+    }else if (err?.name === "CastError") {
+        const simplifiedError = handleCastError(err);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSources = simplifiedError?.errorSources;
+    }else if (err?.code === "11000") {
+        const simplifiedError = handleDuplicateError(err);
         statusCode = simplifiedError?.statusCode;
         message = simplifiedError?.message;
         errorSources = simplifiedError?.errorSources;
