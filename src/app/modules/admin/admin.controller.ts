31e61bd1da6de5430fe9e85bbd7ba32fabadd0f1
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-25 11:03:01 +0600

    admin route controller complete

---
+++ src/app/modules/admin/admin.controller.ts
@@ -0,0 +1,60 @@
+
+import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AdminServices } from './admin.service';
+
+const getSingleAdmin = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await AdminServices.getSingleAdmin(id);
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Admin is retrieved successfully',
+    data: result,
+  });
+});
+
+const getAllAdmins = catchAsync(async (req, res) => {
+  const result = await AdminServices.getAllAdmins(req.query);
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Admins are retrieved successfully',
+    data: result,
+  });
+});
+
+const updateAdmin = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const { admin } = req.body;
+  const result = await AdminServices.updateAdmin(id, admin);
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Admin is updated successfully',
+    data: result,
+  });
+});
+
+const deleteAdmin = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await AdminServices.deleteAdmin(id);
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Admin is deleted successfully',
+    data: result,
+  });
+});
+
+export const AdminControllers = {
+  getAllAdmins,
+  getSingleAdmin,
+  deleteAdmin,
+  updateAdmin,
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,38 +5,39 @@ import { AcademicSemesterRoutes } from "../modules/academicSemester/academicSeme
 import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route";
 import { AcademicDepartmentRoutes } from "../modules/academicDepartment/academicDepartment.route";
 import { FacultyRoutes } from "../modules/faculty/faculty.route";
+import { AdminRoutes } from "../modules/admin/admin.route";
 
 const router = Router();
 
 const moduleRoutes = [
-    {
-        path: '/users',
-        route: UserRoutes,
-      },
-      {
-        path: '/students',
-        route: StudentRoutes,
-      },
-      {
-        path: '/faculties',
-        route: FacultyRoutes,
-      },
-    //   {
-    //     path: '/admins',
-    //     route: AdminRoutes,
-    //   },
-      {
-        path: '/academic-semesters',
-        route: AcademicSemesterRoutes,
-      },
-      {
-        path: '/academic-faculties',
-        route: AcademicFacultyRoutes,
-      },
-      {
-        path: '/academic-departments',
-        route: AcademicDepartmentRoutes,
-      }
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/students',
+    route: StudentRoutes,
+  },
+  {
+    path: '/faculties',
+    route: FacultyRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
+  },
+  {
+    path: '/academic-semesters',
+    route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: AcademicDepartmentRoutes,
+  }
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
