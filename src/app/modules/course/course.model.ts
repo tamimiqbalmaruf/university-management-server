d9591efe13be58c7ae0157fd3c49d68f57e768d6
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-25 18:50:46 +0600

    Created Course Interface Model

---
+++ src/app/modules/course/course.model.ts
@@ -0,0 +1,43 @@
+import { model, Schema, Types } from "mongoose";
+import { TCourse, TPreRequisiteCourses } from "./course.interface";
+
+export const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: "Course"
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+const courseSchema = new Schema<TCourse>({
+    title: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true
+    },
+    prefix: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    code: {
+        type: Number,
+        trim: true,
+        required: true
+    },
+    credits: {
+        type: Number,
+        trim: true,
+        required: true
+    },
+    preRequisiteCourses: [preRequisiteCoursesSchema]
+
+});
+
+
+export const Course = model<TCourse>("Course", courseSchema);
