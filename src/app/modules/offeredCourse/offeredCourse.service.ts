d5f1ef9642c80d0661ce1651f923dd20b7c3d42e
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-12-12 11:59:21 +0600

    Resolve time conflictions for a faculty

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -75,6 +75,27 @@ const createOfferedCourse = async (payload: TOfferedCourse) => {
     }
 
 
+    const assignedSchedules = await OfferedCourse.find({semesterRegistration, faculty, days: {$in: days}}).select("days startTime endTime");
+
+    const newSchedule = {
+        days,
+        startTime,
+        endTime
+    };
+
+
+    assignedSchedules.forEach((schedule) => {
+const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
+const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
+const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
+const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
+
+
+if(newStartTime < existingEndTime && newEndTime > existingStartTime)
+    throw new AppError(StatusCodes.CONFLICT, `This faculty is not available at that time ! Choose other time or day`)
+    })
+
+
     const result = await OfferedCourse.create({ ...payload, academicSemester });
     return result;
 };
