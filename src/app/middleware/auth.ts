bd44c0b1e4b48754d09696cccac936d6183affa8
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-12-16 15:31:11 +0600

    Verify token using auth

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -2,18 +2,29 @@ import { NextFunction, Request, Response } from "express"
 import catchAsync from "../utils/catchAsync";
 import AppError from "../errors/AppError";
 import { StatusCodes } from "http-status-codes";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
 
 const auth = () => {
 
     return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
-    const token = req.headers.authorization;
+        const token = req.headers.authorization;
 
-    if(!token){
-        throw new AppError(StatusCodes.UNAUTHORIZED, "You're not authorized!")
-    }
-        
-        next()
+        if (!token) {
+            throw new AppError(StatusCodes.UNAUTHORIZED, "You're not authorized!")
+        };
+
+        const isTokenValidate = jwt.verify(token, config.jwt_access_secret as string, function (err, decoded) {
+            if (err) {
+                throw new AppError(StatusCodes.UNAUTHORIZED, "You're not authorized!")
+            }
+
+            req.user = decoded as JwtPayload;
+            next();
+        })
+
+
     })
 };
 
