ab90bc6f30a9cf00a36b3c3952b39fc6798daa75
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-12-17 11:33:11 +0600

    Implement password update and track timestamp

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { User } from "../user/user.model";
 import { TChangePassword, TLoginUser } from "./auth.interface";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../../config";
+import bcrypt from "bcrypt";
 
 
 const loginUser = async (payload: TLoginUser) => {
@@ -45,17 +46,44 @@ const loginUser = async (payload: TLoginUser) => {
 
 const changePassword = async (userData: JwtPayload, payload: TChangePassword) => {
 
+    const user = await User.isUserExistsByCustomId(userData.userId);
+
+    if (!user) {
+        throw new AppError(StatusCodes.NOT_FOUND, "The user is not found!")
+    };
+
+    if (user?.isDeleted === true) {
+        throw new AppError(StatusCodes.FORBIDDEN, "The user is deleted!")
+    };
+
+    if (user?.status === "blocked") {
+        throw new AppError(StatusCodes.FORBIDDEN, "The user is blocked!")
+    };
+
+    if (!await User.isPasswordMatched(payload?.oldPassword, user?.password)) {
+        throw new AppError(StatusCodes.FORBIDDEN, "User password is not matched!")
+    };
+
+
+    const newHashedPassword = await bcrypt.hash(
+        payload.newPassword,
+        Number(config.bcrypt_salt_rounds),
+    );
+
+
     await User.findOneAndUpdate(
         {
-          id: userData.userId,
-          role: userData.role,
+            id: userData.userId,
+            role: userData.role,
         },
-        // {
-        //   password: newHashedPassword,
-        //   needsPasswordChange: false,
-        //   passwordChangedAt: new Date(),
-        // },
-      );
+        {
+            password: newHashedPassword,
+            needsPasswordChange: false,
+            passwordChangedAt: new Date(),
+        },
+    );
+
+    return null;
 };
 
 export const AuthServices = {
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -19,6 +19,9 @@ const userSchema = new Schema<TUser, UserModel>(
       type: Boolean,
       default: true,
     },
+    passwordChangedAt: {
+      type: Date
+    },
     role: {
       type: String,
       enum: ['admin', 'student', 'faculty'],
