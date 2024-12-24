ad45a7179f9076458be86915b8002068b82746a3
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-12-24 12:52:58 +0600

    Install multer ,cloudinary and upload an image using an image link

---
+++ src/app/utils/sendImageToCloudinary.ts
@@ -0,0 +1,53 @@
+import { v2 as cloudinary } from 'cloudinary';
+import config from '../config';
+
+export const sendImageToCloudinary = async (imageName: string, path: string) => {
+
+
+
+
+
+
+
+    // Configuration
+    cloudinary.config({
+        cloud_name: config.cloudinary_cloud_name,
+        api_key: config.cloudinary_api_key,
+        api_secret: config.cloudinary_api_secret
+    });
+
+    // Upload an image
+    const uploadResult = await cloudinary.uploader
+        .upload(
+            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+            public_id: 'shoes',
+        }
+        )
+        .catch((error) => {
+            console.log(error);
+        });
+
+    console.log(uploadResult);
+
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url('shoes', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+
+    console.log(optimizeUrl);
+
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url('shoes', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+
+    console.log(autoCropUrl);
+
+
+
+
+}
