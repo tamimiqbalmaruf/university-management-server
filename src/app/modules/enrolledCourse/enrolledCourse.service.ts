28137af3b7f6c51cf966ee775671f8eb78066a68
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-12-28 12:27:05 +0600

    Add a student into enrolled course with more business login and  session

--- src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -5,6 +5,7 @@ import { TEnrolledCourse } from './enrolledCourse.interface';
 import AppError from '../../errors/AppError';
 import EnrolledCourse from './enrolledCourse.model';
 import { Student } from '../student/student.model';
+import mongoose from 'mongoose';
 
 const createEnrolledCourseIntoDB = async (
   userId: string,
@@ -37,10 +38,51 @@ const createEnrolledCourseIntoDB = async (
 
   if (isStudentAlreadyEnrolled) {
     throw new AppError(StatusCodes.CONFLICT, 'Student is already enrolled !');
+  };
+
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const result = await EnrolledCourse.create([{
+
+      semesterRegistration: isOfferedCourseExists.semesterRegistration,
+      academicSemester: isOfferedCourseExists.academicSemester,
+      academicFaculty: isOfferedCourseExists.academicFaculty,
+      academicDepartment: isOfferedCourseExists.academicDepartment,
+      offeredCourse: offeredCourse,
+      course: isOfferedCourseExists.course,
+      student: student._id,
+      faculty: isOfferedCourseExists.faculty,
+      isEnrolled: true,
+
+    }], { session });
+
+    if (!result) {
+      throw new AppError(
+        StatusCodes.BAD_REQUEST,
+        'Failed to enroll in this course !',
+      );
+    }
+
+    const maxCapacity = isOfferedCourseExists.maxCapacity;
+
+    await OfferedCourse.findByIdAndUpdate(offeredCourse, {
+      maxCapacity: maxCapacity - 1,
+    }, {session});
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return result;
+
+  } catch (err: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(err);
   }
-
-
-
 };
 
 
