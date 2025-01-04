cf26e0a93da8f96438a7a5911857e676804cfd7e
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-01-04 23:04:16 +0600

    refactored super admin control

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import validateRequest from '../../middleware/validateRequest';
 import { AcademicFacultyValidations } from './academicFaculty.validation';
 import { AcademicFacultyControllers } from './academicFaculty.controller';
+import auth from '../../middleware/auth';
 
 
 
 const router = express.Router();
 
-router.post('/create-academic-faculty', validateRequest(AcademicFacultyValidations.createAcademicFacultyValidationSchema), AcademicFacultyControllers.createAcademicFaculty);
+router.post('/create-academic-faculty',
+    auth('superAdmin', 'admin'),
+    validateRequest(AcademicFacultyValidations.createAcademicFacultyValidationSchema), AcademicFacultyControllers.createAcademicFaculty);
 
 router.get('/:id', AcademicFacultyControllers.getSingleAcademicFaculty);
 
